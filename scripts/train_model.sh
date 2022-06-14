21cc6bc0d8ac635ffbb0cf12ee30e4e38ec3e49d
nsfzyzz <yqyang1991@gmail.com>
2022-06-14 00:02:17 -0700

    update

--- scripts/train_model.sh
+++ scripts/train_model.sh
@@ -5,9 +5,10 @@
 #SBATCH --cpus-per-task=8   # number of cores per task
 #SBATCH --gres=gpu:1        # number of GPUs (should match -n)
 ##SBATCH --nodelist=ace,manchester,bombe,como,pavia,luigi,zanino    # if you need specific nodes
-#SBATCH --exclude=blaze,flaminio,freddie,r[1-6,8-16],havoc,steropes,atlas
+#SBATCH --nodelist=ace
+##SBATCH --exclude=blaze,flaminio,freddie,r[1-6,8-16],havoc,steropes,atlas
 #SBATCH -t 1-00:00          # time requested (D-HH:MM)
-#SBATCH -D /home/eecs/rkunani/NLP_metrics      # working directory
+#SBATCH -D /data/yyaoqing/Good_vs_bad_data/NLP_metrics_Simpson
 #SBATCH -o slurm_logs/slurm.%N.%j..out # STDOUT
 #SBATCH -e slurm_logs/slurm.%N.%j..err # STDERR
 pwd
@@ -20,14 +21,14 @@ export PYTHONUNBUFFERED=1
 
 srun -N 1 -n 1 python training_script.py \
 --num_of_epochs 20 \
---dataset_name IWSLT \
+--dataset_name WMT14 \
 --language_direction G2E \
 --subsampling --num-samples $1 \
 --num-layers $2 \
 --lr-inverse-dim \
 --lr-factor $3 \
 --dropout $4 \
---checkpoint-path /work/rkunani/pytorch-transformer/checkpoint/IWSLT_sample$1_depth$2_lr$3_dropout$4
+--checkpoint-path /work/yyaoqing/Good_vs_bad_data/checkpoint/NMT_epochs/Simpson/IWSLT_sample$1_depth$2_lr$3_dropout$4
 
 wait
 date
