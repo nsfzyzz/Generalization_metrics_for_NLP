4644c09f4e0b3fa96e880e16cb4a0c1ebc7668fe
Ubuntu <ubuntu@ip-172-31-18-207.ec2.internal>
2022-06-17 23:47:33 +0000

    update

--- scripts/compute_metric.sh
+++ scripts/compute_metric.sh
@@ -4,10 +4,10 @@
 #SBATCH -n 1                # number of tasks (i.e. processes)
 #SBATCH --cpus-per-task=8   # number of cores per task
 #SBATCH --gres=gpu:1        # number of GPUs (should match -n)
-##SBATCH --nodelist=havoc    # if you need specific nodes
-#SBATCH --exclude=como,manchester,blaze,flaminio,freddie,r[1-6,8-16],havoc,steropes,atlas
+#SBATCH --nodelist=manchester    # if you need specific nodes
+##SBATCH --exclude=como,manchester,blaze,flaminio,freddie,r[1-6,8-16],havoc,steropes,atlas
 #SBATCH -t 1-00:00          # time requested (D-HH:MM)
-#SBATCH -D /home/eecs/rkunani/NLP_metrics      # working directory
+#SBATCH -D /data/yyaoqing/Good_vs_bad_data/NLP_metrics_Simpson      # working directory
 #SBATCH -o slurm_logs/slurm.%N.%j..out # STDOUT
 #SBATCH -e slurm_logs/slurm.%N.%j..err # STDERR
 pwd
@@ -23,7 +23,7 @@ $1 \
 --result_suffix "robust_measures.pkl" \
 --epochs 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 \
 --width 512 \
---dataset IWSLT \
+--dataset WMT \
 --num-samples $2 \
 --test_robust_measures \
 --calculate_margin \
