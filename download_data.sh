e7a991f8baa15a1651a53016cff532d9d99256fc
nsfzyzz <yqyang1991@gmail.com>
2022-10-17 00:18:44 -0700

    update

--- download_data.sh
+++ download_data.sh
@@ -1,15 +1,15 @@
 ### Download the generalization metric results for the checkpoints ###
-#curl -O https://zenodo.org/record/7136098/files/checkpoint.zip
+curl -O https://zenodo.org/record/7136098/files/checkpoint.zip
 unzip checkpoint.zip -d checkpoint/
 
 ### Download the rank correlation results for the checkpoints ###
-#curl -O https://zenodo.org/record/7136714/files/results.zip
+curl -O https://zenodo.org/record/7136714/files/results.zip
 unzip results.zip
 
 ### Download the scatter plots ###
-#curl -O https://zenodo.org/record/7136704/files/plots.zip
+curl -O https://zenodo.org/record/7136704/files/plots.zip
 unzip plots.zip -d plots/
 
 ### Download the processed data for Transformer training ###
-#curl -O https://zenodo.org/record/7134119/files/data.zip
+curl -O https://zenodo.org/record/7134119/files/data.zip
 unzip data.zip
